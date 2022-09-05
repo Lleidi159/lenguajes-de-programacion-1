1db94df2782f1c0a09f14e51b4085853246b3b08
Juan Diego Marquez Chaparro <111793633+Lleidi159@users.noreply.github.com>
2022-09-05 15:04:42 -0600

    clase 5 de septiembre del 2022

---
+++ caracteresAcentos.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main(void)
+{
+    printf("caracteres especiales\n");
+
+    printf("%c ",   160);
+    printf("%c \n", 181);
+    printf("%c ",   130);
+    printf("%c \n", 144);
+    printf("%c ",   161);
+    printf("%c \n", 214);
+    printf("%c ",   162);
+    printf("%c \n", 224);
+    printf("%c ",   163);
+    printf("%c \n", 233);
+    printf("%c ",   164);
+    printf("%c \n", 165);
+
+    return 0;
+}
